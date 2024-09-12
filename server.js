5cf50efaa0a5670df9531141a15ab50ff067cc43
Morgan <mgregg14@ivytech.edu>
2024-09-12 18:47:32 -0400

    requests and responses

--- server.js
+++ server.js
@@ -3,8 +3,28 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
+
+    //header
     res.setHeader('Content-Type', 'text/html');
-    fs.readFile('./views/index.html', (err, data) => {
+
+    let path ='./views/';
+    switch(req.url){
+        case '/':
+        path+= 'index.html';
+        break;
+
+        case '/about':
+        path+= 'about.html';
+        break;
+
+        default:
+            path+= '404.html';
+            break;
+    }
+
+//add html file
+
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.log(err);
             res.end();
