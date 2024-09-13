a6a0518c0d280803fd9ff3c23c5ee6443a0fc241
Morgan <mgregg14@ivytech.edu>
2024-09-13 10:41:41 -0400

    about page

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express();
 //set up listen for requests
 app.listen(3000);
 
+
 app.get('/', (req, res) => {
     res.send ('<p >home page </p>');
+});
+
+app.get('/about', (req, res) => {
+    res.send ('<p >about page </p>');
 });
