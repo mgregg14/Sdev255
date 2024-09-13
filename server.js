cbe31ea3186fe7031e1b8115c2c04a172160b428
Morgan <mgregg14@ivytech.edu>
2024-09-13 09:52:19 -0400

    redirect

--- server.js
+++ server.js
@@ -9,16 +9,26 @@ const server = http.createServer((req, res) => {
 
     let path ='./views/';
     switch(req.url){
+
         case '/':
         path+= 'index.html';
+        res.statusCode = 200;
         break;
 
         case '/about':
-        path+= 'about.html';
-        break;
+            path+= 'about.html';
+            res.statusCode = 200;
+            break;
+
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
 
         default:
             path+= '404.html';
+            res.statusCode = 404;
             break;
     }
 
@@ -29,6 +39,8 @@ const server = http.createServer((req, res) => {
             console.log(err);
             res.end();
         } else {
+
+            // res.write
             res.end(data);
         }
     });
