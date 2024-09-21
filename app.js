910074fa0a3209f9fea13d1fe70dc8e0479555ba
Morgan <mgregg14@ivytech.edu>
2024-09-21 17:46:04 -0400

    create ejs

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
 const express = require('express');
 
+// express app
 const app = express();
 
-app.set('view engine', 'ejs');
-
-//set up listen for requests
+// listen for requests
 app.listen(3000);
 
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
 
 app.get('/', (req, res) => {
-    //res.send ('<p >home page </p>');
-    res.render('index');
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    //res.send ('<p >about page </p>');
-    res.render('about');
+  res.render('about', { title: 'About' });
 });
 
-//redirect
-
-app.get('/about-us', (req,res)=>{
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
 });
 
-//404
-
-app.use((req,res) =>{
-    res.status(404).render('404');
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
 });
