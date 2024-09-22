b9614f67c08c597c90125cfb1de30ef58ef3fbb9
Morgan <mgregg14@ivytech.edu>
2024-09-22 15:00:23 -0400

    css

--- app.js
+++ app.js
@@ -3,34 +3,33 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Blog = require('./models/blog');
 
-
 const app = express();
 
-// connect to mongodb
+// Connect to MongoDB
 const dbURI = "mongodb+srv://adminmo:ADMINMO@nodenuts.3gtmy.mongodb.net/node-tut?retryWrites=true&w=majority&appName=Nodenuts";
 
 mongoose.connect(dbURI)
   .then(result => app.listen(3000))
   .catch(err => console.log(err));
 
-
 app.set('view engine', 'ejs');
 
-
+// Middleware
 app.use(express.static('public'));
 app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true })); 
 app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
 
-
+// Routes
 app.get('/add-blog', (req, res) => {
   const blog = new Blog({
     title: 'new blog',
     snippet: 'about my new blog',
     body: 'more about my new blog'
-  })
+  });
 
   blog.save()
     .then(result => {
@@ -69,9 +68,22 @@ app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
 
-// blog routes
+// Blog creation route
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
+});
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog.save()
+    .then(result => {
+      res.redirect('/blogs');
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('Error creating blog');
+    });
 });
 
 app.get('/blogs', (req, res) => {
