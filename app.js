d3349b324511f492672d8149f7eabd8c14d37703
Morgan <mgregg14@ivytech.edu>
2024-09-22 11:57:12 -0400

    changing app pg

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 
 // express app
 const app = express();
-
 const dbURI = 'mongodb+srv://adminmo:<ADMINMO>@nodenuts.3gtmy.mongodb.net/node-tuts?retryWrites=true&w=majority&appName=Nodenuts';
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -16,6 +16,23 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(morgan('dev'));
+
+
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+      title: 'new blog',
+      snippet: 'about my new blog',
+      body: 'more about my new blog'
+    })
+
+    blog.save()
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 app.use((req, res, next) => {
   console.log('new request made:');
