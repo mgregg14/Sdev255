c535863c4c91fa000700d0ecafb4d36925d4aba1
Morgan <mgregg14@ivytech.edu>
2024-09-22 10:13:33 -0400

    mongoose

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
 
 // express app
 const app = express();
-app.listen(3000);
+
+const dbURI = 'mongodb+srv://adminmo:<ADMINMO>@nodenuts.3gtmy.mongodb.net/node-tuts?retryWrites=true&w=majority&appName=Nodenuts';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(morgan('dev'));
 
 app.use((req, res, next) => {
   console.log('new request made:');
